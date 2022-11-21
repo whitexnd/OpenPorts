a5dbba30d1624e1e34a61e08fb7c18edde4cb21e
wh1teXnd <wh1texnd@localhost.localdomain>
2022-11-21 18:28:57 +0100

    new logo

--- puertosabiertosfunction.sh
+++ puertosabiertosfunction.sh
@@ -1,13 +1,24 @@
-function puertosabiertos(){
-        echo -e "\n[+] Analizando Puertos\n"
-        if [ -z "$1" ]; then
-                for port in $(cat /proc/net/tcp | awk '{print $2}' | grep -oP ":[A-Z,0-9,]{4}" | sed "s/://" | sort -u); do
-                        echo -e "\t[*] Port $port -> $(echo "obase=10; ibase=16; $port" | bc)"
-                done
-        else
-                for port in $(cat $1 | awk '{print $2}' | grep -oP ":[A-Z,0-9,]{4}" | sed "s/://" | sort -u); do
-                        echo -e "\t[*] Port $port -> $(echo "obase=10; ibase=16; $port" | bc)"
-                done
-        fi
-        echo -e "\n"
+function openports () {
+	tput setaf 2
+	echo "
+________                      __________              __          
+\_____  \  _____   ____   ____\______   \____________/  |_  ______
+ /   |   \/____ \_/ __ \ /    \|     ___/  _ \_  __ \   __\/  ___/
+/    |    \  |_> >  ___/|   |  \    |  (  <_> )  | \/|  |  \___ \ 
+\_______  /   __/ \___  >___|  /____|   \____/|__|   |__| /____  >
+        \/|__|        \/     \/                                \/ 
+"
+	if [ -z "$1" ]
+	then
+		for port in $(cat /proc/net/tcp | awk '{print $2}' | grep -oP ":[A-Z,0-9,]{4}" | sed "s/://" | sort -u)
+		do
+			echo -e "\t$(tput setaf 148)[*] $(tput setaf 7) Port $port -> $(echo "obase=10; ibase=16; $port" | bc)"
+		done
+	else
+		for port in $(cat $1 | awk '{print $2}' | grep -oP ":[A-Z,0-9,]{4}" | sed "s/://" | sort -u)
+		do
+			echo -e "\t$(tput setaf 148)[*] $(tput setaf 7) Port $port -> $(echo "obase=10; ibase=16; $port" | bc)"
+		done
+	fi
+	echo -e "\n"
 }
